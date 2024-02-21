83b2c4ccb7a5a31758bfdcc892f24d7de5063deb
Sergio Fernández <DrumSergio@users.noreply.github.com>
2024-02-22 00:41:07 +0100

    First steps of dockerizing

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11-slim 
+# lxml not working in 3.12 gcc build-essential pkg-config python3-dev 
+WORKDIR /app
+RUN apt-get update && \
+    apt-get install -y libxml2-dev libxslt-dev chromium && \
+    apt-get clean
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
+EXPOSE 80
+CMD ["python", "src/main.py"]
--- src/main.py
+++ src/main.py
@@ -8,8 +8,12 @@ import time
 import shutil
 import re
 
-def get_sitemap(language):
-    requests_cache.install_cache(language + '/sitemap')
+locationdir = "/data/jw_org/"
+language = "es"
+fulldir = locationdir + language
+
+def get_sitemap():
+    requests_cache.install_cache(fulldir + '/sitemap')
     session = requests_cache.CachedSession('sitemap', expire_after=86400) # 24h
     jw_org = "https://www.jw.org/"
     links = []
@@ -18,7 +22,8 @@ def get_sitemap(language):
     url_locs = soup.find_all("loc")
     for url in url_locs:
         links.append(url.text)
-    return links
+    return ["https://www.jw.org/es/biblioteca/videos/#es/mediaitems/FeaturedLibraryVideos/docid-502018518_1_VIDEO"]
+    #return links
 
 def download_video(url, filename):
     with requests.get(url, stream=True) as r:
@@ -29,38 +34,40 @@ def download_webpage(url, driver):
     print(url)
     driver.get(url)
     local_url = urlparse(url)._replace(netloc="", scheme="")
-    local_folder = urlunparse(local_url).removeprefix("/")
+    local_folder = fulldir + urlunparse(local_url)
     if not os.path.exists(local_folder):
         os.makedirs(local_folder)
+    time.sleep(2)
     page = BeautifulSoup(driver.page_source, "html.parser")
     # video page
     video = page.find("video")
-    if video is not None:
+    if video:
         video_url = video["src"]
         video_filename = os.path.basename(urlparse(video_url).path)
         local_video_path = local_folder + "/" + video_filename
         print(local_video_path)
         download_video(video_url, local_video_path)
         video["src"] = "/" + local_video_path
+        local_folder_name = local_folder + ".html" # If it's a video, do not add /index.html
+    else:
+        local_folder_name = local_folder + "/index.html" 
     # href modification
     for tag_name, attribute_name in [("a", "href"), ("link", "href"), ("base", "href")]:
         for tag in page.find_all(tag_name, **{attribute_name: True}):
             if tag[attribute_name].startswith("https://www.jw.org/"):
                 tag[attribute_name] = tag[attribute_name].replace("https://www.jw.org", "")
-    with open(local_folder + "index.html", "w", encoding="utf-8") as file:
+    with open(local_folder_name, "w", encoding="utf-8") as file:
         file.write(str(page))
 
 if __name__ == '__main__':
-    language = "es"
-    if not os.path.exists(language):
-        os.makedirs(language)
-    links = get_sitemap(language)
+    if not os.path.exists(fulldir):
+        os.makedirs(fulldir)
+    links = get_sitemap()
     options = uc.ChromeOptions()
-    #options.headless = True
+    options.headless = True
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = uc.Chrome(options=options)
+    driver = uc.Chrome(options=options, version_main=121)
     for link in links:
         download_webpage(link, driver)
-        time.sleep(1)
 #download_webpage("https://www.jw.org/es/biblioteca/videos/#es/mediaitems/FeaturedLibraryVideos/docid-502018518_1_VIDEO", driver)
